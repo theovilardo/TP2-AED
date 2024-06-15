6dfa58baaee5f0a8b4ea41ad34d2a3bcea386f9e
theovilardo <theovilardo@gmail.com>
2024-06-15 20:51:14 -0300

    agrego lo que tnemos hasta ahora de la funcion "SistemaSIU" (el constructor))

--- TPGrupal 2/src/main/java/aed/SistemaSIU.java
+++ TPGrupal 2/src/main/java/aed/SistemaSIU.java
@@ -1,6 +1,7 @@
 package aed;
 
 public class SistemaSIU {
+    private Trie<Trie<Materia>> carreras;
 
     enum CargoDocente{
         AY2,
@@ -9,8 +10,21 @@ public class SistemaSIU {
         PROF
     }
 
+    @SuppressWarnings("unchecked") //se saca esto despues
     public SistemaSIU(InfoMateria[] infoMaterias, String[] libretasUniversitarias){
-        throw new UnsupportedOperationException("Método no implementado aún");	    
+        Trie carreras = new Trie<Trie<Materia>>();
+        Trie estudiantes = new Trie<Integer>();
+        for (InfoMateria infoMateria : infoMaterias){
+            ParCarreraMateria[] pares = infoMateria.getParesCarreraMateria();
+            Materia mdata = new Materia();
+            Trie trieMaterias = new Trie<Materia>();
+            for (ParCarreraMateria par : pares){
+                carreras.insertar(par.getCarrera(), trieMaterias); //guardar en un alista todos los tries de materias de carreras que comparte
+                //no deberia ir primero el trieMaterias?
+                trieMaterias.insertar(par.nombreMateria, mdata);
+
+            }
+        }
     }
 
     public void inscribir(String estudiante, String carrera, String materia){
