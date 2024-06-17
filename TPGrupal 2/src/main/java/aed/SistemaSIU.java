7e9e9f8ee3fa323105c52b6c952d4e2eab63ca22
theovilardo <theovilardo@gmail.com>
2024-06-17 19:54:50 -0300

    Update SistemaSIU.java
    Con esta version pasa hasta el test listado_materias()

--- TPGrupal 2/src/main/java/aed/SistemaSIU.java
+++ TPGrupal 2/src/main/java/aed/SistemaSIU.java
@@ -4,6 +4,7 @@ import aed.SistemaSIU.CargoDocente;
 
 public class SistemaSIU {
     private Trie<Trie<Materia>> carreras;
+    private Trie<Estudiante> estudiantes;
 
     enum CargoDocente{
         AY2,
@@ -12,48 +13,62 @@ public class SistemaSIU {
         PROF
     }
 
-    @SuppressWarnings("unchecked")
-    public SistemaSIU(InfoMateria[] infoMaterias, String[] libretasUniversitarias){
-        Trie carrerasNew = new Trie<Trie<Materia>>();
-        Trie trieMaterias = new Trie<Materia>(); // habia que inicializarlo antes del ciclo, no adentro
-        //Trie estudiantes = new Trie<Integer>(); //empiezo a creer que esto no va aca
-        for (InfoMateria infoMateria : infoMaterias){
-            ParCarreraMateria[] paresCarreraMateria = infoMateria.getParesCarreraMateria();
-            Materia mdata = new Materia();
-            // Trie trieMaterias = new Trie<Materia>();
-            for (ParCarreraMateria parCarreraMateria : paresCarreraMateria){
-                trieMaterias.insertar(parCarreraMateria.nombreMateria, mdata);
-                carrerasNew.insertar(parCarreraMateria.getCarrera(), trieMaterias); //guardar en una lista todos los tries de materias de carreras que comparte - coment para Teo
+    public SistemaSIU(InfoMateria[] infoMaterias, String[] libretasUniversitarias) {
+        //Trie newCarreras = new Trie<Trie<Materia>>();
+        carreras = new Trie<>(); //pruebo sin copiar todo a carreras post-ciclo
+
+        for (InfoMateria infoMateria : infoMaterias) { // veo cada infoMateria, vendria a ser la "asignatura"
+            Materia materiaCompartida = new Materia(); // esta instancia de materia se comparte dentro del infomateria (porquq todos los pares dentor de este infomateria especifico correspondend a la misma asignatura)
+            for (ParCarreraMateria parCarreraMateria : infoMateria.getParesCarreraMateria()) { // obtengo los pares carrera-materia
+                String materiaNombre = parCarreraMateria.getNombreMateria(); // guarod la carrera
+                String carreraNombre = parCarreraMateria.getCarrera(); // guardo la materia
+
+                Trie<Materia> materiasDeCarrera = carreras.obtenerSignificado(carreraNombre); //trie pra todas las materias de la carrera
+                if (materiasDeCarrera == null) { //si no existe la carrera (PARA DEBUG).
+                    materiasDeCarrera = new Trie<>(); // nuevo trie
+                    carreras.insertar(carreraNombre, materiasDeCarrera); //insertar materias de carreara en la carrera
+                }
+                materiasDeCarrera.insertar(materiaNombre, materiaCompartida); // insertar la istancia d emateria en las mismas materias
             }
         }
-        carreras = carrerasNew;
+        //carreras = newCarreras;
+        // falta un bloque donde meta a los estudiantes de libretasUniversitarias
+        estudiantes = new Trie<>();
+        for (String libreta : libretasUniversitarias){ //debe estar mal la complejidad, asi que a chequear a la chequeria
+            Estudiante e = new Estudiante();
+            estudiantes.insertar(libreta, e);
+            Estudiante anotarInscripcion = estudiantes.obtenerSignificado(libreta);
+            anotarInscripcion.estudianteSeInscribio();
+        }
     }
 
     public void inscribir(String estudiante, String carrera, String materia){
         Materia materiaParaInscribir = new Materia();
         //busco en el trie de la carrera el trie de la materia y pido su valor tipo materia para inscribir al estudiante
-        if (carreras.obtenerSignificado(carrera) != null){
+        if (carreras.obtenerSignificado(carrera) != null){  // no deberian aparece nulos, se tiene que encargar el constructor (si jode la complejidad se lo saca)
             Trie<Materia> materiasDeLaCarrera = carreras.obtenerSignificado(carrera);
             if (materiasDeLaCarrera.obtenerSignificado(materia) != null){
                 materiaParaInscribir = materiasDeLaCarrera.obtenerSignificado(materia);
             }
         }
         materiaParaInscribir.inscribirEstudiante();
+        //registrar en el trie de estudiantes: (puede que esto haya que tratarlo como las instancias de materias para no alterar la complejiad)
+        //estudiantes.insertar(estudiante, ); // complejidad: O(1) \\mejor armar otra clase para estudiante con un metodo para aumentar al estudiante dado.
     }
 
     public void agregarDocente(CargoDocente cargo, String carrera, String materia){
         //carreras.obtenerSignificado(carrera).obtenerSignificado(materia).agregarDocente(cargo, 1);
         if (cargo == CargoDocente.AY2){
-            carreras.obtenerSignificado(carrera).obtenerSignificado(materia).agregarDocente(3);
+            carreras.obtenerSignificado(carrera).obtenerSignificado(materia).agregarDocente(3); //AY2
         }
         if (cargo == CargoDocente.AY1){
-            carreras.obtenerSignificado(carrera).obtenerSignificado(materia).agregarDocente(2);
+            carreras.obtenerSignificado(carrera).obtenerSignificado(materia).agregarDocente(2); //AY1
         }
         if (cargo == CargoDocente.JTP){
-            carreras.obtenerSignificado(carrera).obtenerSignificado(materia).agregarDocente(1);
+            carreras.obtenerSignificado(carrera).obtenerSignificado(materia).agregarDocente(1); //JTP
         }
         if (cargo == CargoDocente.PROF){
-            carreras.obtenerSignificado(carrera).obtenerSignificado(materia).agregarDocente(0);
+            carreras.obtenerSignificado(carrera).obtenerSignificado(materia).agregarDocente(0); //PROF
         }
     }
 
@@ -62,7 +77,7 @@ public class SistemaSIU {
     }
 
     public void cerrarMateria(String materia, String carrera){
-        throw new UnsupportedOperationException("Método no implementado aún");   
+        throw new UnsupportedOperationException("Método no implementado aún");
     }
 
     public int inscriptos(String materia, String carrera){
@@ -79,28 +94,50 @@ public class SistemaSIU {
     }
 
     public boolean excedeCupo(String materia, String carrera){
-        int [] plantelDocente = carreras.obtenerSignificado(carrera).obtenerSignificado(materia).obtenerPlantelDocente();
-        int estudiantesInscriptos = carreras.obtenerSignificado(carrera).obtenerSignificado(materia).obtenerInscriptos();
-        // cap maximas por cargo docente:
-        int capacidadPorProfesor = 250;
-        int capacidadPorJTP = 100;
-        int capacidadPorAy1 = 20;
-        int capacidadPorAy2 = 30;
+        int estudiantesInscriptos = inscriptos(materia, carrera);
+        int cupo = calcularCupo(carrera, materia);
+        //int cupo = minimo();
         // capacidad total, seria el cupo maximo
-        int capacidadTotal = (plantelDocente[0] * capacidadPorProfesor) + (plantelDocente[1] * capacidadPorJTP) + (plantelDocente[2] * capacidadPorAy1) + (plantelDocente[3] * capacidadPorAy2);
-        
-        return estudiantesInscriptos > capacidadTotal; // flase si excede el cupo
+        //int capacidadTotal = (plantelDocente[0] * capacidadPorProfesor) + (plantelDocente[1] * capacidadPorJTP) + (plantelDocente[2] * capacidadPorAy1) + (plantelDocente[3] * capacidadPorAy2);
+        return estudiantesInscriptos > cupo; // false si excede el cupo
+    }
+
+    // calcula el minimo cupo a exceder
+    public int calcularCupo(String nombreCarrera, String nombreMateria) {
+        Materia materiaAChequear = carreras.obtenerSignificado(nombreCarrera).obtenerSignificado(nombreMateria);
+        int cupoPROF = 250 * materiaAChequear.obtenerPlantelDocente()[0]; //250 * docentes(CargoDocente.PROF, carrera, nombreMateria);
+        int cupoJTP = 100 * materiaAChequear.obtenerPlantelDocente()[1];
+        int cupoAY1 = 20 * materiaAChequear.obtenerPlantelDocente()[2];
+        int cupoAY2 = 30 * materiaAChequear.obtenerPlantelDocente()[3];
+
+        //buscar el minimo
+        int cupo = Math.min(Math.min(cupoPROF, cupoJTP), Math.min(cupoAY1, cupoAY2)); // ver si afecta la complejidad ya que es una comparacion simple (creo)
+
+        return cupo;
+    }
+
+    //auxiliar para devolver el minimo
+    private int minimo(int[] cantidades){
+        int min = cantidades[0];
+        for (int i = 1; i < cantidades.length; i++) {
+            if (cantidades[i] < min) {
+                min = cantidades[i];
+            }
+        }
+        return min;
     }
 
     public String[] carreras(){
-        throw new UnsupportedOperationException("Método no implementado aún");	    
+        String[] stringArray = carreras.claves().toArray(new String[0]); //pasa de List<String> a String[], no creo que afecte la complejidad
+        return stringArray;
     }
 
     public String[] materias(String carrera){
-        throw new UnsupportedOperationException("Método no implementado aún");	    
+        String[] stringToArray = carreras.obtenerSignificado(carrera).claves().toArray(new String[0]);
+        return stringToArray;
     }
 
     public int materiasInscriptas(String estudiante){
-        throw new UnsupportedOperationException("Método no implementado aún");	    
+        return estudiantes.obtenerSignificado(estudiante).cantidadDeInscripciones();
     }
 }
