8212c7be0563a198264e2ac27591a4b72bd41cc9
theovilardo <theovilardo@gmail.com>
2024-06-18 18:39:47 -0300

    Ultima version Estudiante.java

--- TPGrupal 2/src/main/java/aed/Estudiante.java
+++ TPGrupal 2/src/main/java/aed/Estudiante.java
@@ -4,14 +4,19 @@ public class Estudiante {
     private int cantidadDeInscripciones;
 
     public Estudiante(){
-        this.cantidadDeInscripciones = 1;
+        this.cantidadDeInscripciones = 0;
     }
 
-    public void estudianteSeInscribio(){
+    public void estudianteSeInscribio(String materia){
         cantidadDeInscripciones = cantidadDeInscripciones + 1;
     }
 
     public int cantidadDeInscripciones(){
         return cantidadDeInscripciones;
+    }
+
+    // tu materia ya no existe, chau
+    public void dejarMateria(){
+        cantidadDeInscripciones = cantidadDeInscripciones - 1;
     }
 }
