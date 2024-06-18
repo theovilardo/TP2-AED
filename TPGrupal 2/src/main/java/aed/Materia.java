e6e6584b985b35f386865daeb172cb35465803dc
theovilardo <theovilardo@gmail.com>
2024-06-18 18:38:26 -0300

    ultima version de Materia.java

--- TPGrupal 2/src/main/java/aed/Materia.java
+++ TPGrupal 2/src/main/java/aed/Materia.java
@@ -1,18 +1,29 @@
 package aed;
 
+import java.util.ArrayList;
+
 public class Materia{
+    private boolean estaCerrada;
     private int estudiantesInscriptos;
     private int[] plantelDocente;
-    private Trie<Materia>[] carlosMenem; // un trie por cada carrera en la que se compaerte es materia, este podria ser el que apunta a las correlaciones entre carrera y materia
+    private ArrayList<String> estudiantesDeLaMateria; //ArrayList<String> estudiantesDeLaMateria;
+    //private Trie<Materia>[] carlosMenem; // un trie por cada carrera en la que se compaerte es materia, este podria ser el que apunta a las correlaciones entre carrera y materia
 
     public Materia(){
+        this.estaCerrada = false;
         this.estudiantesInscriptos = 0;
         this.plantelDocente = new int[4];
-        this.carlosMenem = null;
+        this.estudiantesDeLaMateria = new ArrayList<>();
+        //this.carlosMenem = null;
     }
 
-    public void inscribirEstudiante(){
+    public void inscribirEstudiante(String estudiante){
         estudiantesInscriptos = estudiantesInscriptos + 1;
+        estudiantesDeLaMateria.add(estudiante);
+    }
+
+    public ArrayList<String> obtenerEstudiantes(){
+        return estudiantesDeLaMateria;
     }
 
     public int obtenerInscriptos(){
@@ -25,6 +36,15 @@ public class Materia{
 
     public void agregarDocente(int cargo){
         //debe sumar la "cantidad" al valor de la posicion indicada por "cargo"
-        plantelDocente[cargo] += 1;
+        plantelDocente[cargo] = plantelDocente[cargo] + 1;
     }
+
+    public void cerrarEstaMateria(){
+        estaCerrada = true;
+    }
+
+    public boolean estaCerrada(){
+        return estaCerrada;
+    }
+
 }
