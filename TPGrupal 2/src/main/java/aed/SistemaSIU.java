03daffbf0a3f2985fbce898135cbe0e0faf24ad1
theovilardo <theovilardo@gmail.com>
2024-06-18 23:39:24 -0300

    agrego comentarios a cada funcion y pasos

--- TPGrupal 2/src/main/java/aed/SistemaSIU.java
+++ TPGrupal 2/src/main/java/aed/SistemaSIU.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 import aed.SistemaSIU.CargoDocente;
 
 public class SistemaSIU {
-    private Trie<Trie<Materia>> carreras;
-    private Trie<Estudiante> estudiantes;
+    private Trie<Trie<Materia>> carreras; // Trie de carreras que guarda en su valor un Trie distinto para las materias de esas carreras
+    private Trie<Estudiante> estudiantes; // Trie de estudianes, sus claves son las libretas universitarias y el valor de cada una es una instancia de Estudiante que guarda las inscripciones a las materias
 
     enum CargoDocente{
         AY2,
@@ -15,48 +15,47 @@ public class SistemaSIU {
         PROF
     }
 
+    // Constructor del SistemaSIU
     public SistemaSIU(InfoMateria[] infoMaterias, String[] libretasUniversitarias) {
-        //Trie newCarreras = new Trie<Trie<Materia>>();
-        carreras = new Trie<>(); //pruebo sin copiar todo a carreras post-ciclo
+        carreras = new Trie<>(); // Creo una nueva instancia para el Trie de carreras
 
         for (InfoMateria infoMateria : infoMaterias) { // veo cada infoMateria, vendria a ser la "asignatura"
-            Materia materiaCompartida = new Materia(); // esta instancia de materia se comparte dentro del infomateria (porquq todos los pares dentor de este infomateria especifico correspondend a la misma asignatura)
+            Materia materiaCompartida = new Materia(); // esta instancia de materia se comparte dentro del infoMateria (porque todos los pares dentro de este infoMateria especifico corresponden a la misma asignatura)
             for (ParCarreraMateria parCarreraMateria : infoMateria.getParesCarreraMateria()) { // obtengo los pares carrera-materia
-                String materiaNombre = parCarreraMateria.getNombreMateria(); // guarod la carrera
-                String carreraNombre = parCarreraMateria.getCarrera(); // guardo la materia
+                String materiaNombre = parCarreraMateria.getNombreMateria(); // guardo el nombre de la carrera
+                String carreraNombre = parCarreraMateria.getCarrera(); // guardo el nombre de la materia
 
-                Trie<Materia> materiasDeCarrera = carreras.obtenerSignificado(carreraNombre); //trie pra todas las materias de la carrera
-                if (materiasDeCarrera == null) { //si no existe la carrera (PARA DEBUG).
-                    materiasDeCarrera = new Trie<>(); // nuevo trie
-                    carreras.insertar(carreraNombre, materiasDeCarrera); //insertar materias de carreara en la carrera
+                Trie<Materia> materiasDeCarrera = carreras.obtenerSignificado(carreraNombre); // Trie para guardar todas las materias de la carrera
+                if (materiasDeCarrera == null) { // si no existe la carrera (PARA DEBUG, ver si puede sacarse esta guarda).
+                    materiasDeCarrera = new Trie<>(); // Nuevo Trie en caso de que no exista
+                    carreras.insertar(carreraNombre, materiasDeCarrera); // insertar el Trie materiasDeCarrera en la carrera
                 }
-                materiasDeCarrera.insertar(materiaNombre, materiaCompartida); // insertar la istancia d emateria en las mismas materias
+                materiasDeCarrera.insertar(materiaNombre, materiaCompartida); // insertar la istancia compartida de materia en las mismas materias (esto es para que apunten a la misma instancia de Materia y se actualicen juntas, es un alisaing intencional)
             }
         }
         
-        estudiantes = new Trie<>();
+        estudiantes = new Trie<>(); // Creo una nueva instancia para el Trie de estudiantes
         for (String libreta : libretasUniversitarias) {
-            Estudiante e = new Estudiante();
-            estudiantes.insertar(libreta, e);
+            Estudiante e = new Estudiante(); // Inicializo la clase estudiante para guardarlo como valor para cada clave (libreta universitaria) en el Trie de estudiantes
+            estudiantes.insertar(libreta, e); // inserto la libreta como clave y el e: Estudiante como valor
         }
     }
 
     public void inscribir(String estudiante, String carrera, String materia){
         Materia materiaParaInscribir = new Materia();
         //busco en el trie de la carrera el trie de la materia y pido su valor tipo materia para inscribir al estudiante
-        if (carreras.obtenerSignificado(carrera) != null){  // no deberian aparece nulos, se tiene que encargar el constructor (si jode la complejidad se lo saca)
+        if (carreras.obtenerSignificado(carrera) != null){  // no deberian aparece nulos, se tiene que encargar el constructor (si jode la complejidad se lo saca la guarda)
             Trie<Materia> materiasDeLaCarrera = carreras.obtenerSignificado(carrera);
             if (materiasDeLaCarrera.obtenerSignificado(materia) != null){
                 materiaParaInscribir = materiasDeLaCarrera.obtenerSignificado(materia);
             }
         }
         materiaParaInscribir.inscribirEstudiante(estudiante);
-        //registrar en el trie de estudiantes: (puede que esto haya que tratarlo como las instancias de materias para no alterar la complejiad)
+        //registrar la inscripcion en el trie de estudiantes:
         estudiantes.obtenerSignificado(estudiante).estudianteSeInscribio(materia); // nose si esta bien
     }
 
     public void agregarDocente(CargoDocente cargo, String carrera, String materia){
-        //carreras.obtenerSignificado(carrera).obtenerSignificado(materia).agregarDocente(cargo, 1);
         if (cargo == CargoDocente.AY2){
             carreras.obtenerSignificado(carrera).obtenerSignificado(materia).agregarDocente(3); //AY2
         }
@@ -77,14 +76,13 @@ public class SistemaSIU {
 
     public void cerrarMateria(String materia, String carrera){
         ArrayList<String> clavesEstudiante = carreras.obtenerSignificado(carrera).obtenerSignificado(materia).obtenerEstudiantes();
-    
-        // while i < clavesEstudiante
-        //     estu
-        for (String libreta : clavesEstudiante){ // no funciona con arrayList -> fix: .toArray(new String[0])
+
+        for (String libreta : clavesEstudiante){
             estudiantes.obtenerSignificado(libreta).dejarMateria();
         }
-        //pruebo desmarcnado la materia como abierta:
+        // Al cerrar la carrera la marco como cerrada y la elimino del Trie
         carreras.obtenerSignificado(carrera).obtenerSignificado(materia).cerrarEstaMateria();
+        // no hay una explicacion de porque lo hacemos por separado, ver que poner aca
         carreras.obtenerSignificado(carrera).eliminarSignficado(materia); //elimino la instancia de la Materia
         carreras.obtenerSignificado(carrera).eliminar(materia); // elimino la clave
     }
@@ -93,9 +91,7 @@ public class SistemaSIU {
         int inscriptos = 0;
         if (carreras.obtenerSignificado(carrera) != null){
             Trie<Materia> materiasDeLaCarrera = carreras.obtenerSignificado(carrera);
-            // esta siendo null? :
-            //inscriptos = materiasDeLaCarrera.obtenerSignificado(materia).obtenerInscriptos();
-            if (materiasDeLaCarrera.obtenerSignificado(materia) != null){ // esto esta null y no deberia
+            if (materiasDeLaCarrera.obtenerSignificado(materia) != null){ // chequear si es necesaria la guarda
                 inscriptos = materiasDeLaCarrera.obtenerSignificado(materia).obtenerInscriptos();
             }
         }
@@ -105,36 +101,34 @@ public class SistemaSIU {
     public boolean excedeCupo(String materia, String carrera){
         int estudiantesInscriptos = inscriptos(materia, carrera);
         int cupo = calcularCupo(carrera, materia);
-        return estudiantesInscriptos > cupo; // false si excede el cupo, segun el TAD tenia que ser >=, pero asi los tests se rompen asi que queda >
+        return estudiantesInscriptos > cupo; // false si excede el cupo, segun el TAD tenia que ser >=, pero asi los tests se rompen asi que queda >, ver que poner bien aca
     }
 
-    // calcula el minimo cupo a exceder
+    // Funcion Auxilar para excedeCupo(), calcula el minimo cupo a exceder.
     public int calcularCupo(String nombreCarrera, String nombreMateria) {
         Materia materiaAChequear = carreras.obtenerSignificado(nombreCarrera).obtenerSignificado(nombreMateria);
-        int cupoPROF = 250 * materiaAChequear.obtenerPlantelDocente()[0]; //250 * docentes(CargoDocente.PROF, carrera, nombreMateria);
+        int cupoPROF = 250 * materiaAChequear.obtenerPlantelDocente()[0];
         int cupoJTP = 100 * materiaAChequear.obtenerPlantelDocente()[1];
         int cupoAY1 = 20 * materiaAChequear.obtenerPlantelDocente()[2];
         int cupoAY2 = 30 * materiaAChequear.obtenerPlantelDocente()[3];
 
         //buscar el minimo
-        int cupo = Math.min(Math.min(cupoPROF, cupoJTP), Math.min(cupoAY1, cupoAY2)); // ver si afecta la complejidad ya que es una comparacion simple (creo)
+        int cupo = Math.min(Math.min(cupoPROF, cupoJTP), Math.min(cupoAY1, cupoAY2)); // ver si afecta la complejidad ya que es una comparacion simple (creo), y ver si se podia usar el Math o habira que usar otra aux: Minimo
 
         return cupo;
     }
 
     public String[] carreras(){
-        String[] stringArray = carreras.claves().toArray(new String[0]); //pasa de List<String> a String[], no creo que afecte la complejidad
+        String[] stringArray = carreras.claves().toArray(new String[0]); //pasa de List<String> a String[], no creo que afecte la complejidad, igual chequear
         return stringArray;
     }
 
     public String[] materias(String carrera){
-        // String[] stringToArray = carreras.obtenerSignificado(carrera).claves().toArray(new String[0]);
-        String[] stringToArray = carreras.obtenerSignificado(carrera).claveMaterias().toArray(new String[0]); //pruebo la version de claves que chequea el estado de la Materia
+        String[] stringToArray = carreras.obtenerSignificado(carrera).claveMaterias().toArray(new String[0]); // .claveMaterias() devuelve la lista de materias de la carrera (mientras esten abiertas)
         return stringToArray;
     }
 
     public int materiasInscriptas(String estudiante){
-        //le reste uno para probar y funciono, habra que ver porque siempre esta saliendo uno de mas (mi ssospecha es que esta anotanod al mismo estudiante mas de una vez, lo que seria un problema porque habria que diferenciar or estudiante)
         return estudiantes.obtenerSignificado(estudiante).cantidadDeInscripciones();
     }
 }
