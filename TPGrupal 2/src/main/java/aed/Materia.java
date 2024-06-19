8bb580385392f7663c6d0aed45fe46e9eb9a5d4f
theovilardo <theovilardo@gmail.com>
2024-06-18 23:04:16 -0300

    agrego los comentarios para cada funcion

--- TPGrupal 2/src/main/java/aed/Materia.java
+++ TPGrupal 2/src/main/java/aed/Materia.java
@@ -3,46 +3,51 @@ package aed;
 import java.util.ArrayList;
 
 public class Materia{
-    private boolean estaCerrada;
-    private int estudiantesInscriptos;
-    private int[] plantelDocente;
-    private ArrayList<String> estudiantesDeLaMateria; //ArrayList<String> estudiantesDeLaMateria;
-    //private Trie<Materia>[] carlosMenem; // un trie por cada carrera en la que se compaerte es materia, este podria ser el que apunta a las correlaciones entre carrera y materia
+    private boolean estaCerrada; // Estado de la Asignatura (Abierta o Cerrada)
+    private int estudiantesInscriptos; // Cantidad de estudiantes inscriptos
+    private int[] plantelDocente; // Cantidad de Docentes en cada posicion segun su cargo
+    private ArrayList<String> estudiantesDeLaMateria; // Lista de libretas de los estudiantes inscriptos en la materia
 
+    // Constructor de Materia
     public Materia(){
         this.estaCerrada = false;
         this.estudiantesInscriptos = 0;
         this.plantelDocente = new int[4];
         this.estudiantesDeLaMateria = new ArrayList<>();
-        //this.carlosMenem = null;
     }
 
+    // Inscribe al estudiante en la materia (aumenta la cantidad de inscriptos y guarda la libreta del estudiante)
     public void inscribirEstudiante(String estudiante){
         estudiantesInscriptos = estudiantesInscriptos + 1;
         estudiantesDeLaMateria.add(estudiante);
     }
 
+    // Devuelve la lista de libretas de los estudiantes inscriptos
     public ArrayList<String> obtenerEstudiantes(){
         return estudiantesDeLaMateria;
     }
 
+    // Devuelve la cantidad de estudiantes inscriptos
     public int obtenerInscriptos(){
         return estudiantesInscriptos;
     }
 
+    // Devuelve la cantidad de docentes en cada posicion segun su cargo
     public int[] obtenerPlantelDocente(){
         return plantelDocente;
     }
 
+    // Agrega un docente al cargo seleccionado (0 o 1 o 2 o 3)
     public void agregarDocente(int cargo){
-        //debe sumar la "cantidad" al valor de la posicion indicada por "cargo"
         plantelDocente[cargo] = plantelDocente[cargo] + 1;
     }
 
+    // Cierra la materia marcando el estaCerrada como true
     public void cerrarEstaMateria(){
         estaCerrada = true;
     }
 
+    // Devuelve True si la materia esta cerrada
     public boolean estaCerrada(){
         return estaCerrada;
     }
