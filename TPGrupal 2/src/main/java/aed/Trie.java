926e5c12a374eb45b5e26726e64f0754c74d7b07
theovilardo <theovilardo@gmail.com>
2024-06-18 23:23:40 -0300

    agrego comentarios a las funciones

--- TPGrupal 2/src/main/java/aed/Trie.java
+++ TPGrupal 2/src/main/java/aed/Trie.java
@@ -5,83 +5,86 @@ import java.util.Collections;
 import java.util.List;
 
 public class Trie<T>{
-    private Nodo raiz;
+    private Nodo raiz; // Nodo raiz del Trie
     public static final int ALFABETO_LEXICOGRAFICO = 256; // alfabeto para el orden lexicografico segun el codigo ASCII
 
-    // clase interna del nodo
+    // Clase interna del Nodo
     private class Nodo{
         private ArrayList<Nodo> hijos;
         private T valor;
 
-        //uso: si el nodo es el nodo de significado su valor se llena con los datos, sino su valor queda vacio
+        // NOTA: Los nodos cuyo valor es vacion son parte de la clave y cobran sentido al llegar la nodo significado que es el que contiene el valor de la clave
         public Nodo(T valor) {
             this.hijos = new ArrayList<>(Collections.nCopies(ALFABETO_LEXICOGRAFICO, null));
             this.valor = valor;
         }
 
+        // Devuelve el valor del Nodo
         public T obtenerValor() {
             return valor;
         }
 
+        // Asigna un valor al Nodo (le da signfiicado al Nodo y por lo tanto a la clave misma)
         public void asignarValor(T valor) {
             this.valor = valor;
         }
     }
 
+    // Constructor del Trie
     public Trie() {
         this.raiz = new Nodo(null);
     }
-    // ver como no pisar otros caracteres
-    //Insertar
-    // Dato: si se mete una clave ya existente reemplazara el valor actual por el dado
+
+    // Insertar una clave con su respectivo valor en el Trie
+    // NOTA: si se mete una clave ya existente reemplazara el valor actual por el dado, es decir se sobreescribe.
     public void insertar(String clave, T valor) {
-        //clave = Normalizador.normalizar(clave); //normaliza la clave en caso de tener caracteres fuera dle codigo ASCII 128 como el "ó". Falta corroborar
         Nodo actual = raiz;
         for (char c : clave.toCharArray()) { // convierte a cadena de chars
             int index = c; // pasa el char a codiog ASCII
             if (actual.hijos.get(index) == null) {
-                actual.hijos.set(index, new Nodo(null)); //los nodos que no son el de siginficaod tienen valor null por ahora
+                actual.hijos.set(index, new Nodo(null)); //los nodos que no son el de siginficaod se les asigna el valor null por ahora
             }
             actual = actual.hijos.get(index);
         }
         actual.asignarValor(valor);
     }
 
+    // Dada una clave (ya insertada en el Trie) devuelve su valor
     public T obtenerSignificado(String clave){
-        //clave = Normalizador.normalizar(clave); //normaliza la clave
         Nodo actual = raiz;
         for (char c : clave.toCharArray()) {
-            int index = c; // Convertimos el caracter en su valor ASCII
+            int index = c;
             if (actual.hijos.get(index) == null) {
                 return null;
             }
             actual = actual.hijos.get(index);
         }
+        // El proposito de esta guarda es identificar el caso donde el valor de la clave es nulo
         if (actual.obtenerValor() == null){
             throw new UnsupportedOperationException("el valor de esta clave es nulo (no deberia pasar)");
         }
         return actual.obtenerValor();
     }
 
+    // Devuelve True si la clave pertence al Trie (creo que al final ni se uso asi que se lo puede sacar)
     public boolean buscar(String clave) {
-        //clave = Normalizador.normalizar(clave); //normaliza la clave
         Nodo actual = raiz;
-        for (char c : clave.toCharArray()) { // convierte a cadena de chars
-            int index = c; // pasa el char a codiog ASCII
+        for (char c : clave.toCharArray()) {
+            int index = c;
             if (actual.hijos.get(index) == null) {
                 return false;
             }
             actual = actual.hijos.get(index);
         }
-        return actual.valor != null; // si el nodo significado no es nulo encontraste la clave
+        return actual.valor != null; // Si el nodo significado no es nulo, la clave esta en el Trie
     }
 
     // Método para eliminar una clave del Trie
     public void eliminar(String clave) {
-        //clave = Normalizador.normalizar(clave); //normaliza la clave
         eliminarTrie(raiz, clave, 0); //inicializo el eliminador con el index en 0 (primera posicion de la clave)
     }
 
+    // Funcion Auxiliar de eliminar()
     private boolean eliminarTrie(Nodo actual, String clave, int indice){
         //caso base:
         if (actual == null) {
@@ -92,7 +95,7 @@ public class Trie<T>{
             if (actual.obtenerValor() == null) {
                 return false; // La clave no existe en el trie
             }
-            actual.asignarValor(null); // Eliminar el valor del nodo final
+            actual.asignarValor(null); // eliminar el valor del nodo final
             return noTieneHijos(actual);
         }
         //recursion
@@ -112,7 +115,8 @@ public class Trie<T>{
         return false;
     }
 
-    private boolean noTieneHijos(Nodo nodo) { //pred no tiene hijos
+    // Funciona como predicado para saber si el Nodo tiene hijos o no || Complejidad: 
+    private boolean noTieneHijos(Nodo nodo) {
         for (Nodo hijo : nodo.hijos) {
             if (hijo != null) {
                 return false;
@@ -121,20 +125,18 @@ public class Trie<T>{
         return true;
     }
 
-    // obtener todas las claves del trie
+    // Obtiene todas las claves del Trie y devuelve una lista con cada clave en formato String
     public List<String> claves() {
         List<String> claves = new ArrayList<>();
         obtenerClaves(raiz, new StringBuilder(), claves);
         return claves;
     }
 
+    // Funcion Auxiliar de claves()
     private void obtenerClaves(Nodo actual, StringBuilder claveActual, List<String> claves) {
         if (actual == null) {
             return;
         }
-        // if (claveActual.toString() == "Intro a la Programación"){
-        //     throw new UnsupportedOperationException("te encontre hdp");
-        // }
         if (actual.obtenerValor() != null) {
             claves.add(claveActual.toString());
         }
@@ -148,21 +150,24 @@ public class Trie<T>{
         }
     }
 
+    // Elimina el significado de una clave (haciendo que la clave ya no sea valida)
     public void eliminarSignficado(String clave){
         Nodo actual = raiz;
-        for (char c : clave.toCharArray()) { // convierte a cadena de chars
-            int index = c; // pasa el char a codiog ASCII
+        for (char c : clave.toCharArray()) {
+            int index = c;
             actual = actual.hijos.get(index);
         }
         actual.asignarValor(null);
     }
 
+    // Obtiene todas las claves del Trie, pero devuelve una lista con solo las lcaves validas (segunn al estado de la Materia)
     public List<String> claveMaterias() {
         List<String> claves = new ArrayList<>();
         obtenerClavesMaterias(raiz, new StringBuilder(), claves);
         return claves;
     }
 
+    // Funcion Auxiliar para claveMaterias()
     private void obtenerClavesMaterias(Nodo actual, StringBuilder claveActual, List<String> claves) {
         if (actual == null) {
             return;
