b28233f0469778b7ba713aa1aa03048fcb444449
theovilardo <theovilardo@gmail.com>
2024-06-18 23:08:09 -0300

    Agrego comentarios a cada funcion

--- TPGrupal 2/src/main/java/aed/Estudiante.java
+++ TPGrupal 2/src/main/java/aed/Estudiante.java
@@ -1,21 +1,24 @@
 package aed;
 
 public class Estudiante {
-    private int cantidadDeInscripciones;
+    private int cantidadDeInscripciones; // Cantidad de materias a las que esta inscripto el estudiante
 
+    // COnstructor de Estudiante
     public Estudiante(){
         this.cantidadDeInscripciones = 0;
     }
 
+    // Inscribir al estudiante en una materia (aumenta en 1 la cantidad de inscripciones)
     public void estudianteSeInscribio(String materia){
         cantidadDeInscripciones = cantidadDeInscripciones + 1;
     }
 
+    // Devuelve la cantidad de materias a las que esta inscripto el estudiante
     public int cantidadDeInscripciones(){
         return cantidadDeInscripciones;
     }
 
-    // tu materia ya no existe, chau
+    // Resta una inscripcion al estudiante en caso de que deje la materia (se usa en el caso de que la materia cierre)
     public void dejarMateria(){
         cantidadDeInscripciones = cantidadDeInscripciones - 1;
     }
